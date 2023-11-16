157c86e9dbddef6d3125ebd61ce85fb453d3888b
Santiago Garzon <santigg90@gmail.com>
2023-11-16 00:55:38 -0500

    Se agregan actualizaciones de archivos

--- js/main.js
+++ js/main.js
@@ -26,34 +26,28 @@ function restar(cardId) {
 }
 
 function submitForm() {
-    // Obtener valores del formulario
+
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
     const query = document.getElementById('query').value;
 
-    // Expresión regular para validar el correo electrónico
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-    // Validar campos y mostrar mensajes de error si es necesario
     if (!name || !email || !query) {
         showToast('Por favor, completa todos los campos.');
     } else if (!emailRegex.test(email)) {
         showToast('Por favor, ingresa un correo electrónico válido.');
     } else {
-        // Aquí puedes enviar el formulario a tu servidor o realizar otras acciones
         showToast('Consulta enviada con éxito!');
-        // Puedes reiniciar el formulario aquí si es necesario
         document.getElementById('contactForm').reset();
     }
 }
 
 function showToast(message) {
-    // Mostrar mensaje de toast
     const toast = document.getElementById('toast');
     toast.textContent = message;
     toast.style.display = 'block';
 
-    // Ocultar el mensaje después de 3 segundos
     setTimeout(() => {
         toast.style.display = 'none';
     }, 3000);
